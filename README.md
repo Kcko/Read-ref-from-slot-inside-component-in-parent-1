af44e2ddb29946c71706dd184e7d8e05d227c922
Kcko <admin@rjwebdesign.cz>
2022-07-25 20:53:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-pma1t6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-pma1t6)
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <HelloWorld name="child" :b="setRefOnA">
+      <input type="text" ref="a" value="default value of this input" />
+    </HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    setRefOnA(el) {
+      return this.$refs.a;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
